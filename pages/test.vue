749cb00691aad3be91315fc10569a4e7f561991f
Niko Guo <niko.guo@ehealth.com>
2025-07-14 22:37:21 +0800

    feat: Add page /test

---
+++ pages/test.vue
@@ -0,0 +1,15 @@
+<!-- ./pages/index.vue -->
+<template>
+  <div>
+    Test
+  </div>
+</template>
+<script setup>
+onMounted(async () => {
+  test();
+});
+
+function test2() {
+  console.log('test2');
+}
+</script>
